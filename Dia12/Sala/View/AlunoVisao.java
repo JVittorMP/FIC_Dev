1cc854a9a3cab0de32ddfa0a2cfef4baecef7db1
João Vittor Padilha <103512187+JVittorMP@users.noreply.github.com>
2023-06-22 16:22:59 -0400

    Delete Dia12 Repository

--- Dia12/Sala/View/AlunoVisao.java
+++
@@ -1,9 +0,0 @@
-package Dia12.Sala.View;
-
-public class AlunoVisao {
-    public static void exibirDadosAluno(String matricula, String nome, int idade){
-        System.out.println("Matricula: " + matricula);
-        System.out.println("Nome: " + nome);
-        System.out.println("Idade: " + idade);
-    }
-}
